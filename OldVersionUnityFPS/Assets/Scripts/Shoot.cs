cb72f236ed3a8b7de62aa1cce8270be0d9703a39
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2019-02-16 13:49:58 +0300

    + fix OldVersionUnityFPS

---
+++ OldVersionUnityFPS/Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;//подключение бибилиотеки для UI-системы 
+
+public class Shoot : MonoBehaviour {
+
+    private Camera _camera; //Перменная тип Camera
+    // Use this for initialization
+    void Start()
+    {
+        _camera = GetComponent<Camera>();//Доступ к компонентам
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { //реакция на нажатие кнопки мыши
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0); //середина экрана, половина ширины и высоты
+
+        }
+    }
+
+}
